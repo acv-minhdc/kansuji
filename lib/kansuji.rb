bbc56921a5276ac4aab02632c4ad8bc42d2c0f20
Minh <minhdc@atmarkcafe.org>
2018-06-28 18:34:27 +0700

    incomplete to_number

--- lib/kansuji.rb
+++ lib/kansuji.rb
@@ -1,8 +1,4 @@
-class String
-  def to_number; end
-end
-
-$base_kanji = {:char_num => ['', '一', '二', '三', '四', '五', '六', '七', '八', '九'], :char_count => { 2 => '十', 3 => '百', 4 => '千', 5 => '万', 9 => '億', 13 => '兆', 17 => '京', 21 => '垓', 25 => '𥝱', 29 => '穣', 33 => '溝', 37 => '澗', 41 => '正', 45 => '載', 49 => '極', 53 => '恒河沙', 57 => '阿僧祇', 61 => '那由他', 64 => '不可思議', 68 => '無量大数' } }
+$base_kanji = { char_num: ['', '一', '二', '三', '四', '五', '六', '七', '八', '九'], char_count: { 2 => '十', 3 => '百', 4 => '千', 5 => '万', 9 => '億', 13 => '兆', 17 => '京', 21 => '垓', 25 => '𥝱', 29 => '穣', 33 => '溝', 37 => '澗', 41 => '正', 45 => '載', 49 => '極', 53 => '恒河沙', 57 => '阿僧祇', 61 => '那由他', 64 => '不可思議', 68 => '無量大数' } }
 
 def str_num_to_kanji(str)
   return $base_kanji[:char_num][str[0].to_i] if str.length == 1 # stop recursion
@@ -17,5 +13,28 @@ end
 class Numeric
   def to_kansuji
     self == 0 ? '零' : str_num_to_kanji(to_s)
+  end
+end
+
+class String
+  def to_number
+    str = reverse
+    result = ''
+    parse_kanji = $base_kanji[:char_count]
+    str.each_char.with_index do |char, index|
+      if index.even?
+        chart_to_num = $base_kanji[:char_num].find_index(char)
+        result += if chart_to_num
+                    chart_to_num.to_s
+                  else
+                    result.empty? ? '0' : '1'
+                  end
+      else
+        expect_count, expect_char = parse_kanji.first
+        until parse_kanji.shift.include?(char); end
+        result += '0' * (parse_kanji.key(char) - expect_count - 1)
+      end
+    end
+    result.reverse!.to_i
   end
 end
--- spec/lib/kansuji_spec.rb
+++ spec/lib/kansuji_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 require 'kansuji'
 
-RSpec.describe 'Kansuji test' do
-  it 'right' do
+RSpec.describe 'test' do
+  it 'Kansuji test: num -> kanji' do
     expect(111111.to_kansuji).to eq '十一万千百十一'
     expect(75454652352.to_kansuji).to eq '七百五十四億五千四百六十五万二千三百五十二'
     expect(7545465235289756812.to_kansuji).to eq '七百五十四京五千四百六十五兆二千三百五十二億八千九百七十五万六千八百十二'
@@ -10,5 +10,9 @@ RSpec.describe 'Kansuji test' do
     expect((10 ** 20 + 4*10**16).to_kansuji).to eq '垓四京'
     expect((10 ** 24).to_kansuji).to eq '𥝱'
     expect(441354548944654676794465457462156324.to_kansuji).to eq '四千四百十三溝五千四百五十四穣八千九百四十四𥝱六千五百四十六垓七千六百七十九京四千四百六十五兆四千五百七十四億六千二百十五万六千三百二十四'
+  end
+
+  it 'Kansuji test: kanji -> num' do
+    expect('十一万千百十一'.to_number).to eq 111111
   end
 end
