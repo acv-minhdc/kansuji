1c055adb85157069d29a6b14797dc6deca44065e
Minh <minhdc@atmarkcafe.org>
2018-07-02 17:49:52 +0700

    add more case spec

--- lib/kansuji.rb
+++ lib/kansuji.rb
@@ -1,41 +1,50 @@
-module BaseCharKanji
-  class << self; attr_reader :base_kanji end
-  @base_kanji = { char_num: ['', '一', '二', '三', '四', '五', '六', '七', '八', '九'], char_count: { 2 => '十', 3 => '百', 4 => '千', 5 => '万', 9 => '億', 13 => '兆', 17 => '京', 21 => '垓', 25 => '𥝱', 29 => '穣', 33 => '溝', 37 => '澗', 41 => '正', 45 => '載', 49 => '極', 53 => '恒河沙', 57 => '阿僧祇', 61 => '那由他', 64 => '不可思議', 68 => '無量大数' } }
-end
+# frozen_string_literal: true
 
+module Kanji
+  class << self; attr_reader :chars end
+  @chars = { num: %w[零 一 二 三 四 五 六 七 八 九],
+             bcount: { 2 => '十', 3 => '百', 4 => '千', 5 => '万', 9 => '億',
+                       13 => '兆', 17 => '京', 21 => '垓', 25 => '𥝱', 29 => '穣',
+                       33 => '溝', 37 => '澗', 41 => '正', 45 => '載', 49 => '極',
+                       53 => '恒河沙', 57 => '阿僧祇', 61 => '那由他', 64 => '不可思議', 68 => '無量大数' } }
+end
+# Append method to Numeric class
 class Numeric
-  include BaseCharKanji
+  include Kanji
   def to_kansuji
-    self == 0 ? '零' : str_num_to_kanji(to_s)
+    zero? ? '零' : num_to_kanji(to_s)
   end
 
-  private
-
-  def str_num_to_kanji(str)
-    str = str.to_i.to_s # delete leading zero
-    return BaseCharKanji.base_kanji[:char_num][str[0].to_i] if str.length == 1 # stop recursion
+  def num_to_kanji(str)
+    return '' if (str = str.to_i.to_s) == '0'
+    return Kanji.chars[:num][str[0].to_i] if str.length == 1
     count = str.length
-    count -= 1 until BaseCharKanji.base_kanji[:char_count][count]
-    first_char = str_num_to_kanji(str[0, str.length - count + 1])
-    (first_char == '一' ? '' : first_char) + BaseCharKanji.base_kanji[:char_count][count] + str_num_to_kanji(str[str.length - count + 1, count - 1])
+    count -= 1 until Kanji.chars[:bcount][count]
+    first_char = num_to_kanji(str[0, str.length - count + 1])
+    (first_char == '一' ? '' : first_char) + Kanji.chars[:bcount][count]\
+      + num_to_kanji(str[str.length - count + 1, count - 1])
   end
 end
-
+# Append method to class String
 class String
-  include BaseCharKanji
+  include Kanji
   def to_number
-    self == '零' ? 0 : kanji_to_num(self)
+    kanji_to_num(self)
   end
-
-  private
 
   def kanji_to_num(str)
-    return 0 if str.empty?
-    return BaseCharKanji.base_kanji[:char_num].find_index(str) if str.length == 1 && BaseCharKanji.base_kanji[:char_num].include?(str)
-    count_char_collection = str.chars.select { |char| BaseCharKanji.base_kanji[:char_count].value?(char) }
-    raise ArgumentError, 'Invalid format kanji number to convert' if count_char_collection.empty?
-    max_count_char = count_char_collection.max_by { |char| BaseCharKanji.base_kanji[:char_count].key(char) }
-    first_char = str[0, str.index(max_count_char)]
-    (first_char.empty? ? 1 : kanji_to_num(first_char)) * 10**(BaseCharKanji.base_kanji[:char_count].key(max_count_char) - 1) + kanji_to_num(str[str.index(max_count_char) + 1, str.length - str.index(max_count_char) - 1])
+    return 0 if str.empty? || str.nil?
+    return Kanji.chars[:num].find_index(str) if Kanji.chars[:num].include?(str)
+    char_collect = str.chars.select { |chr| Kanji.chars[:bcount].value?(chr) }
+    if char_collect.empty?
+      return str.chars.inject('') do |num, chr|
+        num + (Kanji.chars[:num].include?(chr) ? kanji_to_num(chr).to_s : '')
+      end .to_i
+    end
+    max_count = char_collect.max_by { |chr| Kanji.chars[:bcount].key(chr) }
+    index_max = str.index(max_count)
+    head = (first = str[0, index_max]).empty? ? 1 : kanji_to_num(first)
+    (head.zero? ? 1 : head) * 10**(Kanji.chars[:bcount].key(max_count) - 1)\
+    + kanji_to_num(str[index_max + 1, str.length - index_max - 1])
   end
 end
--- spec/lib/kansuji_spec.rb
+++ spec/lib/kansuji_spec.rb
@@ -1,36 +1,100 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'kansuji'
 
-RSpec.describe 'test' do
-  context 'Kansuji test: num -> kanji' do
-    it 'general examples' do
-      expect(0000001.to_kansuji).to eq '一'
-      expect(111111.to_kansuji).to eq '十一万千百十一'
-      expect(75454652352.to_kansuji).to eq '七百五十四億五千四百六十五万二千三百五十二'
-      expect(7545465235289756812.to_kansuji).to eq '七百五十四京五千四百六十五兆二千三百五十二億八千九百七十五万六千八百十二'
-      expect(0.to_kansuji).to eq '零'
-      expect((10 ** 20 + 4 * 10**16).to_kansuji).to eq '垓四京'
-      expect((10 ** 24).to_kansuji).to eq '𥝱'
-      expect(441354548944654676794465457462156324.to_kansuji).to eq '四千四百十三溝五千四百五十四穣八千九百四十四𥝱六千五百四十六垓七千六百七十九京四千四百六十五兆四千五百七十四億六千二百十五万六千三百二十四'
-    end
-end
-
+RSpec.describe 'Kansuji test lib' do
+  # num -> kanji
   context 'Kansuji test: kanji -> num' do
-    it 'general examples' do
-      expect('十一'.to_number).to eq 11
-      expect('一'.to_number).to eq 1
-      expect('十一万'.to_number).to eq 110000
-      expect('七百五十四億五千四百六十五万二千三百五十二'.to_number).to eq 75454652352
-      expect('零'.to_number).to eq 0
-      expect('七百五十四京五千四百六十五兆二千三百五十二億八千九百七十五万六千八百十二'.to_number).to eq 7545465235289756812
-      expect('垓四京'.to_number).to eq 10 ** 20 + 4 * 10**16
-      expect('四千四百十三溝五千四百五十四穣八千九百四十四𥝱六千五百四十六垓七千六百七十九京四千四百六十五兆四千五百七十四億六千二百十五万六千三百二十四'.to_number).to eq 441354548944654676794465457462156324
-      expect('一十一'.to_number).to eq 11
+    describe 'general case' do
+      it 'remove 零(zero) head before convert' do
+        expect('十一一零'.to_number).to eq 10 + 110
+        expect('零一'.to_number).to eq 1
+        expect('零十一'.to_number).to eq 11
+      end
+
+      it '10^0' do
+        expect('零'.to_number).to eq 0
+        expect('一'.to_number).to eq 1
+        expect('九'.to_number).to eq 9
+      end
+
+      it '10^1' do
+        expect('十'.to_number).to eq 10
+        expect('十一'.to_number).to eq 11
+        expect('十八'.to_number).to eq 18
+        expect('一十八'.to_number).to eq 18
+      end
+
+      it '10^2' do
+        expect('百'.to_number).to eq 100
+        expect('百一'.to_number).to eq 101
+        expect('百十一'.to_number).to eq 111
+      end
+
+      it '10^3' do
+        expect('千'.to_number).to eq 1000
+        expect('千一'.to_number).to eq 1001
+        expect('千七百'.to_number).to eq 1700
+      end
+
+      it '10^10' do
+        expect('')
+      end
+
+      it 'long int' do
+        expect('七百五十四億五千四百六十五万二千三百五十二'.to_number).to eq 75_454_652_352
+        expect('四千四百十三溝五千四百五十四穣八千九百四十四𥝱六千五百四十六垓七千六百七十九\
+        京四千四百六十五兆四千五百七十四億六千二百十五万六千三百二十四'.to_number).to eq\
+          441_354_548_944_654_676_794_465_457_462_156_324
+      end
     end
 
-    it 'still right when bonus 一 before count char' do
-      expect('十一万千百十一'.to_number).to eq 111111
-      expect('一十一万一千一百十一'.to_number).to eq 111111
+    describe 'Speacial cases' do
+      it 'empty string will convert to 0' do
+        expect(''.to_number).to eq 0
+      end
+
+      it 'Ignore strange(non kanji) characters' do
+        expect('!@!##%$#$%#$%$一十八'.to_number).to eq 18
+        expect('!@!##%$十八#$%#$%$'.to_number).to eq 18
+        expect('一!@!##%$十$$$$八#$%#$%$'.to_number).to eq 18
+        expect('!@!##%$_十$$se232535fsefs    efe$$八_#$%#$%$'.to_number).to eq 18
+      end
+
+      it 'Strange characters(no any kanji) will convert to 0' do
+        expect('Hello World 1234567 !@#$%%^&&*'.to_number).to eq 0
+      end
+
+      it 'Short rule' do
+        expect('一二三四五'.to_number).to eq 12_345
+        expect('五四三二一'.to_number).to eq 54_321
+      end
+
+      it 'Mix rule' do
+        expect('十一一'.to_number).to eq 21 # 10 + 11
+        expect('十一一零'.to_number).to eq 10 + 110
+        expect('兆十一一零零'.to_number).to eq 10**12 + 10 + 1100
+      end
+
+      it 'still right when bonus 一 before count char' do
+        expect('十一万千百十一'.to_number).to eq 111_111
+        expect('一十一万一千一百一十一'.to_number).to eq 111_111
+      end
+    end
+  end
+
+  # kanji -> num
+  context 'Kansuji test: num -> kanji' do
+    it 'general cases' do
+      expect(0.to_kansuji).to eq '零'
+      expect(1.to_kansuji).to eq '一'
+      expect(11.to_kansuji).to eq '十一'
+      expect(110_000.to_kansuji).to eq '十一万'
+      expect((10**20 + 4 * 10**16).to_kansuji).to eq '垓四京'
+      expect(441_354_548_944_654_676_794_465_457_462_156_324.to_kansuji).to\
+        eq '四千四百十三溝五千四百五十四穣八千九百四十四𥝱六千五百四十六垓七千六百七十九京四\
+        千四百六十五兆四千五百七十四億六千二百十五万六千三百二十四'
     end
   end
 end
